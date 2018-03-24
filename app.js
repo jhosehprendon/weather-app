2b5e4ca554adb3235a49f1908b0df55dcc3be3a2
Jhosehp <jhosehp.rendon@gmail.com>
2018-03-24 16:13:50 -0500

    Added error handling

--- app.js
+++ app.js
@@ -14,11 +14,21 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
     
-    let encodedAddress = encodeURIComponent(argv.a);
+    var encodedAddress = encodeURIComponent(argv.a);
 
 request({
     url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyCT6nxqAPZ9gBcPgBNvGhUkb25m6zQyRiM`,
     json: true
 }, (error, response, body) => {
-    console.log(`Longitud: ${body.results[0].geometry.location.lat}`);
+
+    if(error) {
+        console.log('Unable to connect to Google server');
+    } else if(body.status === 'ZERO_RESULTS') { 
+        console.log('Unable to find that address');
+    } else if(body.status === 'OK') {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitud: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitud: ${body.results[0].geometry.location.lng}`);
+    }
+   
 });
