744b84a586310acca743580938c5a5ff98249ef5
Jhosehp <jhosehp.rendon@gmail.com>
2018-03-24 17:21:43 -0500

    Wired up wether search

--- app.js
+++ app.js
@@ -1,34 +1,44 @@
-const request = require('request');
-const yargs = require('yargs');
+// const yargs = require('yargs');
 
-const argv = yargs
-    .options({
-        a: {
-            demand: true,
-            alias: "address",
-            describe: 'Address to fetch weather for',
-            string: true
-        }
-    })
-    .help()
-    .alias('help', 'h')
-    .argv;
+// const geocode = require('./geocode/geocode.js');
+
+// const argv = yargs
+//     .options({
+//         a: {
+//             demand: true,
+//             alias: "address",
+//             describe: 'Address to fetch weather for',
+//             string: true
+//         }
+//     })
+//     .help()
+//     .alias('help', 'h')
+//     .argv;
+
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//     if (errorMessage) {
+//         console.log(errorMessage);
+//     } else {
+//         console.log(JSON.stringify(results, undefined, 2))
+//     }
+// });
     
-    var encodedAddress = encodeURIComponent(argv.a);
+// 704166ba287072bf553537b514b77280
+
+const request = require ('request');
+
 
 request({
-    url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyCT6nxqAPZ9gBcPgBNvGhUkb25m6zQyRiM`,
+    url: 'https://api.darksky.net/forecast/704166ba287072bf553537b514b77280/37.67,-122.4233',
     json: true
 }, (error, response, body) => {
-
     if(error) {
-        console.log('Unable to connect to Google server');
-    } else if(body.status === 'ZERO_RESULTS') { 
-        console.log('Unable to find that address');
-    } else if(body.status === 'OK') {
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Latitud: ${body.results[0].geometry.location.lat}`);
-        console.log(`Longitud: ${body.results[0].geometry.location.lng}`);
+        console.log('Unable to connect to forecast server');
+    } else if (response.satusCode === 400) {
+        console.log('Unable to fetch weather');
+    } else if(!error && response.statusCode === 200) {
+        console.log(body.currently.temperature);
+    } else {
+        console.log('Unable to fetch weather');
     }
-   
 });
