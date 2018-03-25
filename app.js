041b0c91562f17ec1de91dc658643e9e90d71f8c
Jhosehp <jhosehp.rendon@gmail.com>
2018-03-25 10:42:34 -0500

    Changed callbacks to promises :hammer:

--- app.js
+++ app.js
@@ -1,44 +1,39 @@
-// const yargs = require('yargs');
+const yargs = require('yargs');
 
-// const geocode = require('./geocode/geocode.js');
-
-// const argv = yargs
-//     .options({
-//         a: {
-//             demand: true,
-//             alias: "address",
-//             describe: 'Address to fetch weather for',
-//             string: true
-//         }
-//     })
-//     .help()
-//     .alias('help', 'h')
-//     .argv;
-
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//     if (errorMessage) {
-//         console.log(errorMessage);
-//     } else {
-//         console.log(JSON.stringify(results, undefined, 2))
-//     }
-// });
-    
-// 704166ba287072bf553537b514b77280
-
-const request = require ('request');
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
 
-request({
-    url: 'https://api.darksky.net/forecast/704166ba287072bf553537b514b77280/37.67,-122.4233',
-    json: true
-}, (error, response, body) => {
-    if(error) {
-        console.log('Unable to connect to forecast server');
-    } else if (response.satusCode === 400) {
-        console.log('Unable to fetch weather');
-    } else if(!error && response.statusCode === 200) {
-        console.log(body.currently.temperature);
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: "address",
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
     } else {
-        console.log('Unable to fetch weather');
+       console.log(results.address);
     }
+
+    weather.getWeather(results.latitud, results.longitud, (errorMessage, weatherResults) => {
+        if(errorMessage) {
+            console.log(errorMessage)
+        } else {
+            console.log(JSON.stringify(weatherResults, undefined, 2))
+            console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}.`);
+            
+        }
+    })
+
 });
+    
+
